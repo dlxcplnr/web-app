cd0c55fc2cfa8e72e70b2599190ecadeb2496b35
internet <dlxcplnr>
2026-07-03 10:24:00 +0300

    edited /api/config.php

--- api/config.php
+++ api/config.php
@@ -1,16 +1,15 @@
 <?php
-
-$host = 'localhost';
+$host = 'db';          # <-- Звертаємося до контейнера бази за його ім'ям з docker-compose
 $dbname = 'coffee_mood';
 $username = 'root';
-$password = 'root';
+$password = 'super_secret_password_123'; # <-- Вкажи той пароль, що в MYSQL_ROOT_PASSWORD!
 
-$dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
+# Порт 3306 вказувати необов'язково, PDO візьме його за замовчуванням
+$dsn = "mysql:host=$host;dbname=$dbname;charset=utf8"; 
 
 try {
-$pdo = new PDO($dsn, $username, $password);
-$pdo->setAttribute(PDO::ATTR_ERRMODE,
-PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-die('Помилка підключення до БД: ' . $e->getMessage());
+    die('Помилка підключення до БД: ' . $e->getMessage());
 }
